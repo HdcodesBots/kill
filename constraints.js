c281da410c146df31fa7bea9cefbf84aace60f1a
hd123githbub <77619998+hd123githbub@users.noreply.github.com>
2021-03-23 13:57:36 +0530

    Add files via upload

---
+++ constraints.js
@@ -0,0 +1,23 @@
+class rope {
+    constructor(bodyA,pointB){
+    var chain1 = { bodyA: bodyA,
+                   pointB: pointB,
+                   stifness: 0.04,
+                   length: 30
+                   
+    }
+    this.pointB = pointB
+    this.chain2 = Constraint.create(chain1)
+    World.add(world,this.chain2); 
+    }
+    detach(){
+        this.chain2.bodyA = null
+    }
+    display(){
+        if(this.chain2.bodyA){
+    strokeWeight(5)
+    var pointB = this.pointB;
+    line(this.chain2.bodyA.position.x,this.chain2.bodyA.position.y,pointB.x,pointB.y)
+    }
+}
+}
---
+++ superHero.js
@@ -0,0 +1,16 @@
+class man {
+    constructor(x,y,width,height){
+        var options = { restitution: false              
+        }
+    this.body = Bodies.rectangle(x,y,width,height,options)
+    this.width=  width;
+    this.height = height;
+    this.image = loadImage("Superhero-01.png")
+    World.add(world,this.body);
+    }
+    display(){
+    imageMode(CENTER)
+    image(this.image,this.body.position.x,this.body.position.y,this.width,this.height);
+
+    }
+}
